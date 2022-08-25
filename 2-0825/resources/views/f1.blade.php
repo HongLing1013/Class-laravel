92b764b953db901f7ec5192760f0f86be1774a27
unknown <tsubasababy1013@gmail.com>
2022-08-25 08:35:10 +0800

    將昨天的資料夾改為當天日期,練習新增一個路由連到三個頁面

---
+++ 2-0825/resources/views/f1.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <button><a href="/">index</a></button><br>
+  <button><a href="/f1">f1</a></button><br>
+  <button><a href="/f2">f2</a></button><br>
+  <button><a href="/f3">f3</a></button>
+</body>
+</html>
